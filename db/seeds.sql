a6c1886821ff9abe9e16aa612f84221fe27ef7bb
Alex Milroy <amilroy@gmail.com>
2020-10-14 20:55:02 -0700

    built sql schema

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Romanes of the Day Burger");
+INSERT INTO burgers (burger_name) VALUES ("Peas and Thank You Burger");
+INSERT INTO burgers (burger_name) VALUES ("Yes I Cayenne Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Sweet Home Avocado Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Gourdon-Hamsey Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Poblano Picasso Burger", true);
