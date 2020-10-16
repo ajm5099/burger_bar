4d3de0218d5a2799db0790b2f30196b25f8a9168
Alex Milroy <amilroy@gmail.com>
2020-10-15 19:03:41 -0700

    weird file is in my folder

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -34,12 +34,12 @@ router.put("/api/burgers/:id", function(req, res) {
   burger.update({
     devoured: req.body.devoured
   }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
+    // if (result.changedRows == 0) {
+    //   // If no rows were changed, then the ID must not exist, so 404
+    //   return res.status(404).end();
+    // } else {
       res.status(200).end();
-    }
+    // }
   });
 });
 
